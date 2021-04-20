cb11abc69923cbff52887ee095b26a56bec195f2
Brandon Diaz <branal845diaz@gmail.com>
2021-04-20 00:47:35 -0500

    Se creó el modelo de reuniones y sehicieron las respectivas migraciones

--- terrenos_panoramicos/reuniones/apps.py
+++ terrenos_panoramicos/reuniones/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ReunionesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reuniones'
+    verbose_name = 'Reuniones'
--- terrenos_panoramicos/reuniones/models.py
+++ terrenos_panoramicos/reuniones/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Servicio_Reunion(models.Model):
+    servicio = models.CharField('Servicio a tratar', max_length=50)
+
+class Estado_Reunion(models.Model):
+    estado = models.CharField('Estado de la solicitus', max_length=50)
+
+class Reunion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha_oficial= models.ForeignKey('Fechas')
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+
+class Fechas_Reunion(models.Model):
+    id_solicitud = models.ForeignKey('Reunion',on_delete=models.CASCADE)
+    fecha = models.DateTimeField('Fechas', auto_now=False, auto_now_add=False)
+    
