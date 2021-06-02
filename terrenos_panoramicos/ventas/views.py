b6c6e295f9873418cb7b6ee1f7a814dd86eafd38
Brandon Diaz <branal845diaz@gmail.com>
2021-06-01 19:55:18 -0500

    Se agrego a catalogo las clasificaiones requeridas, falta list group

--- terrenos_panoramicos/ventas/views.py
+++ terrenos_panoramicos/ventas/views.py
@@ -24,8 +24,8 @@ from ventas.models import Inmueble
 
 @login_required
 def catalogo_view(request):
-    #Inmuebles con una superfice menor a 250m
-    inmuebles_todos = Inmueble.objects.filter(status__exact='Oferta').all()
+    #Todos los inmuebles
+    inmuebles_todos = Inmueble.objects.filter(status__exact='Oferta')
     #Inmuebles con una superfice menor a 250m
     inmuebles_sup_menos250 = Inmueble.objects.filter(Q(status__exact='Oferta'), Q(surface__lt = 250))
     #Inmuebles con una frente mayor a 10m
@@ -38,16 +38,14 @@ def catalogo_view(request):
     inmuebles_Totprecio_menos1m = Inmueble.objects.filter(Q(status__exact='Oferta'), Q(totalprice__lt = 100000))
     #Inmueble con con regimen proiedad privada
     inmuebles_rp_privada = Inmueble.objects.filter(Q(status__exact='Oferta'), Q(regimen_propiedad__exact = 'Privada'))
-    #inmueble con regimen proviedad ejidL
+    #inmueble con regimen proviedad ejidal
     inmuebles_rp_ejidal = Inmueble.objects.filter(Q(status__exact='Oferta'), Q(regimen_propiedad__exact = 'Ejidal'))
     #Inmueble con regimen propiedad comunal
     inmuebles_rp_comunal = Inmueble.objects.filter(Q(status__exact='Oferta'), Q(regimen_propiedad__exact = 'Comunal'))
     #Inmuebles con uso de suelo residencial
     inmuebles_us_residencial = Inmueble.objects.filter(Q(status__exact='Oferta'), Q(uso_suelo__exact = 'Residencial'))
     #Inmuebles con uso de suelo actividades productivas
-    inmuebles_us_ap = Inmueble.objects.filter(Q(status__exact='Oferta'), Q(uso_suelo__exact = 'Residencial'))
-    #Inmuebles con uso de suelo residencial
-    inmuebles_us_residencial = Inmueble.objects.filter(Q(status__exact='Oferta'), Q(uso_suelo__exact = 'Actividades Productivas'))
+    inmuebles_us_ap = Inmueble.objects.filter(Q(status__exact='Oferta'), Q(uso_suelo__exact = 'Actividades Productivas'))
     #Inmuebles con uso de suelo Equipamiento
     inmuebles_us_equipamiento = Inmueble.objects.filter(Q(status__exact='Oferta'), Q(uso_suelo__exact = 'Equipamiento'))
     #Inmuebles con uso de suelo Infraestructura
@@ -67,21 +65,20 @@ def catalogo_view(request):
             Q(totalprice__icontains=querset)
         ).distinct() 
 
-    return render(request, 'ventas/catalogo.html', {inmuebles_todos:'inmuebles_todos',
-                                                    inmuebles_sup_menos250:'inmuebles_sup_menos250',
-                                                    inmuebles_front_mas10:'inmuebles_front_mas10',
-                                                    inmuebles_bot_mas15:'inmuebles_bot_mas15',
-                                                    inmuebles_precio_menos20k:'inmuebles_precio_menos20k',
-                                                    inmuebles_Totprecio_menos1m:'inmuebles_Totprecio_menos1m',
-                                                    inmuebles_rp_privada:'inmuebles_rp_privada',
-                                                    inmuebles_rp_ejidal:'inmuebles_rp_ejidal',
-                                                    inmuebles_rp_comunal:'inmuebles_rp_comunal',
-                                                    inmuebles_us_residencial:'inmuebles_us_residencial',
-                                                    inmuebles_us_ap:'inmuebles_us_ap',
-                                                    inmuebles_us_residencial:'inmuebles_us_residencial',
-                                                    inmuebles_us_equipamiento:'inmuebles_us_equipamiento',
-                                                    inmuebles_us_infraestructura:'inmuebles_us_infraestructura',
-                                                    inmuebles_us_averde:'inmuebles_us_averde',
+    return render(request, 'ventas/catalogo.html', {'inmuebles_todos':inmuebles_todos,
+                                                    'inmuebles_sup_menos250':inmuebles_sup_menos250,
+                                                    'inmuebles_front_mas10':inmuebles_front_mas10,
+                                                    'inmuebles_bot_mas15':inmuebles_bot_mas15,
+                                                    'inmuebles_precio_menos20k':inmuebles_precio_menos20k,
+                                                    'inmuebles_Totprecio_menos1m':inmuebles_Totprecio_menos1m,
+                                                    'inmuebles_rp_privada':inmuebles_rp_privada,
+                                                    'inmuebles_rp_ejidal':inmuebles_rp_ejidal,
+                                                    'inmuebles_rp_comunal':inmuebles_rp_comunal,
+                                                    'inmuebles_us_residencial':inmuebles_us_residencial,
+                                                    'inmuebles_us_ap':inmuebles_us_ap,
+                                                    'inmuebles_us_equipamiento':inmuebles_us_equipamiento,
+                                                    'inmuebles_us_infraestructura':inmuebles_us_infraestructura,
+                                                    'inmuebles_us_averde':inmuebles_us_averde,
                                                     })
 
 def me_interesa(request):
