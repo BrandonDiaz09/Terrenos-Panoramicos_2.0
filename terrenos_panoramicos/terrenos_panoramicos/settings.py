70f8ff021f34d0e6c4b74fb8c9f31b46144d5bae
Brandon Diaz <branal845diaz@gmail.com>
2021-06-02 02:38:42 -0500

    Se agrego una base de datos remota MySQL

--- terrenos_panoramicos/terrenos_panoramicos/settings.py
+++ terrenos_panoramicos/terrenos_panoramicos/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.db.backends.mysql.base import DatabaseWrapper
+DatabaseWrapper.data_types['DateTimeField'] = 'datetime' # fix for MySQL 5.5
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,8 +84,12 @@ WSGI_APPLICATION = 'terrenos_panoramicos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sql5416508',
+        'USER': 'sql5416508',
+        'PASSWORD': 'UCzKCWLTls',
+        'HOST': 'sql5.freesqldatabase.com',
+        'PORT': '3306',
     }
     }
 
