19c5fd19ba13336cfb90544b810707f399e955a5
Brandon Diaz <branal845diaz@gmail.com>
2021-06-04 15:39:28 -0500

    Se agrego el diseño de Conocenos

--- terrenos_panoramicos/terrenos_panoramicos/settings.py
+++ terrenos_panoramicos/terrenos_panoramicos/settings.py
@@ -93,18 +93,23 @@ WSGI_APPLICATION = 'terrenos_panoramicos.wsgi.application'
 #         'PORT': '3306',
 #     }
 #     }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'sql5416508',
+#         'USER': 'sql5416508',
+#         'PASSWORD': 'UCzKCWLTls',
+#         'HOST': 'sql5.freesqldatabase.com',
+#         'PORT': '3306',
+#     }
+#     }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sql5416508',
-        'USER': 'sql5416508',
-        'PASSWORD': 'UCzKCWLTls',
-        'HOST': 'sql5.freesqldatabase.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
     }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
