419fb09058fa8282a1dfa9ef9662f415d5ad5e7c
Axe Diaz <axel.diaz@kiranalabs.mx>
2023-08-11 13:45:35 -0600

    chages in models

--- terrenos_panoramicos/sig/models.py
+++ terrenos_panoramicos/sig/models.py
@@ -2,13 +2,24 @@ from django.db import models
 from users.models import Profile
 from django.contrib.gis.db import models
 from django.core.validators import MaxLengthValidator
-
-
+from ventas.models import Inmueble
 
 
 class Perimeter(models.Model):
     polygon = models.PolygonField()
+    inmueble = models.ForeignKey(
+        Inmueble,
+        on_delete=models.CASCADE,
+        related_name="perimeter",
+        null=True,
+    )
 
 
 class Position(models.Model):
     point_of_references = models.PointField()
+    inmueble = models.ForeignKey(
+        Inmueble,
+        on_delete=models.CASCADE,
+        related_name="position",
+        null=True,
+    )
--- terrenos_panoramicos/sig/views.py
+++ terrenos_panoramicos/sig/views.py
@@ -7,13 +7,16 @@ import json
 from django.core.serializers import serialize
 from django.views.generic.base import TemplateView
 import random
+from ventas.models import Inmueble, Colindancias
+from sig.models import Perimeter
+
 
 class MarkersMapView(TemplateView):
     template_name = "sig/map.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        geo_data =[]
+        geo_data = []
         geojson = serialize("geojson", geo_data)
         print(geojson)
         context["geo_data"] = json.loads(geojson)
@@ -23,71 +26,98 @@ class MarkersMapView(TemplateView):
 def view_all_geographical_properties(request):
     import random
 
+    try:
+        inmueble_mas_actual = Inmueble.objects.latest("created")
+        # Filter Colindancias instances with the "Datos propietario" status
+        colindancias_prop = Colindancias.objects.filter(
+            inmueble=inmueble_mas_actual, status=Colindancias.DATOS_PROPIETARIO
+        ).first()
+        perimetro = inmueble_mas_actual.perimeter.all().first()
+        colindancias_tv = Colindancias.objects.filter(
+            inmueble=inmueble_mas_actual, status=Colindancias.DATOS_TERRAVISION
+        ).first()
+
+        datos_generales = inmueble_mas_actual
+        if perimetro is not None:
+            perimetro_queryset = Perimeter.objects.filter(pk=perimetro.pk)
+            geojson = serialize("geojson", [perimetro])
+            geojson_data = json.loads(geojson)
+            feature = geojson_data["features"][0]
+            dataState = {"type": "Feature", "geometry": feature["geometry"]}
+        else:
+            perimetro_queryset = []
+            return render(
+                request,
+                "403.html",
+                {},
+            )
+    except Inmueble.DoesNotExist:
+        # Manejar la situación en la que no hay inmuebles en la base de datos
+        return render(
+            request,
+            "403.html",
+            {},
+        )
+    imagenes = {
+        "UBICACIÓN EN MUNICIPIO": inmueble_mas_actual.ubicacion_en_municipio.url
+        if inmueble_mas_actual.ubicacion_en_municipio
+        else "",
+        "CROQUIS DE UBICACIÓN": inmueble_mas_actual.croquis_de_ubicacion.url
+        if inmueble_mas_actual.croquis_de_ubicacion
+        else "",
+        "CROQUIS DEL PREDIO": inmueble_mas_actual.croquis_de_del_predio.url
+        if inmueble_mas_actual.croquis_de_del_predio
+        else "",
+        "FOTO DEL PREDIO": inmueble_mas_actual.foto_del_predio.url
+        if inmueble_mas_actual.foto_del_predio
+        else "",
+        "ORTOFOTO": inmueble_mas_actual.ortofoto.url
+        if inmueble_mas_actual.ortofoto
+        else "",
+        "FOTO DEL POSEDOR ACTUAL": inmueble_mas_actual.foto_de_posedor_actual.url
+        if inmueble_mas_actual.foto_de_posedor_actual
+        else "",
+    }
     dataInmueble = {
         "datosGenerales": {
-            "calle": "Calle Benito Juárez",
-            "Numero Oficial": "123",
-            "Codigo Postal": "52740",
-            "PARAJE": "La Hacienda",
-            "NOMBRE DEL POSEEDOR": "María González",
-            "USO DE SUELO": "Residencial",
-            "SUPERFICIE": "350 m²",
-            "MEDIDAS Y COLINDANCIAS":"", 
-            "NORTE": "Colinda con propiedad de Juan Martínez",
-            "SUR": "Colinda con Calle Morelos",
-            "ORIENTE": "Colinda con propiedad de Ana Rodríguez",
-            "PONIENTE": "Colinda con propiedad de Carlos López"
-            ,
+            "calle": inmueble_mas_actual.street,
+            "Numero Oficial": inmueble_mas_actual.number_of,
+            "Codigo Postal": inmueble_mas_actual.postal_code,
+            "PARAJE": inmueble_mas_actual.paraje,
+            "NOMBRE DEL POSEEDOR": inmueble_mas_actual.user.username,
+            "USO DE SUELO": inmueble_mas_actual.get_uso_suelo_display(),
+            "SUPERFICIE": f"{inmueble_mas_actual.surface} m²",
+            "MEDIDAS Y COLINDANCIAS": "",
+            "NORTE": f"{colindancias_prop.norte} metros",  # Agregar la información real aquí
+            "SUR": f"{colindancias_prop.sur} metros",  # Agregar la información real aquí
+            "ORIENTE": f"{colindancias_prop.este} metros",  # Agregar la información real aquí
+            "PONIENTE": f"{colindancias_prop.oeste} metros",  # Agregar la información real aquí
         },
         "datosMedicion": {
-            "NORTE": f"{random.randint(100, 300)} metros",
-            "SUR": f"{random.randint(100, 300)} metros",
-            "ORIENTE": f"{random.randint(100, 300)} metros",
-            "PONIENTE": f"{random.randint(100, 300)} metros",
-            "SUPERFICIE": f"{random.randint(200, 500)} m²",
-            "NOMBRE DEL ACTUAL POSEEDOR": "Eduardo Ramírez",
+            "NORTE": f"{colindancias_tv.norte} metros",
+            "SUR": f"{colindancias_tv.sur} metros",
+            "ORIENTE": f"{colindancias_tv.este} metros",
+            "PONIENTE": f"{colindancias_tv.oeste} metros",
+            "SUPERFICIE": f"{inmueble_mas_actual.surface} m²",
+            "NOMBRE DEL ACTUAL POSEEDOR": f"{inmueble_mas_actual.user.username}",
             "TOTAL A PAGAR": f"${random.randint(10000, 50000)}",
             "OBSERVACIONES": "Sin observaciones",
         },
-        "imagenes": {
-            "UBICACIÓN EN MUNICIPIO": "https://static.vecteezy.com/system/resources/previews/001/061/391/non_2x/architectural-blueprints-and-blueprint-rolls-with-drawing-instruments-photo.jpg",
-            "CROQUIS DE UBICACIÓN": "https://images.arq.com.mx/thumbnails/4/244331.jpg",
-            "CROQUIS DEL PREDIO": "https://marvich.cl/wp-content/uploads/2019/11/94911bdd6640f0dba85c698d13a7e900.gif",
-            "FOTO DEL PREDIO": "https://images.arq.com.mx/thumbnails/4/244331.jpg",
-            "ORTOFOTO": "https://topografiaygeosistemas.com/wp-content/uploads/2015/05/plano-topografico.jpg",
-            "FOTO DEL POSEDOR ACTUAL": "https://images.arq.com.mx/thumbnails/4/244331.jpg"
-        },
-        "tipo": {
-            "UBICACIÓN EN MUNICIPIO_IMG": "https://images.arq.com.mx/thumbnails/4/244331.jpg"
-        },
-        "datosGen":{
-            "title":"Datos Generales de Medicion",
+        "imagenes": imagenes,
+        "datosGen": {
+            "title": "Datos Generales de Medicion",
             "REALIZO": "Axel Diaz",
             "REVISO": "Esteban Medina",
             "Vo. Bo. COMUNEROS": "Jorge Sanchez",
         },
-        "Nota": "EL PRESENTE DOCUMENTO Y LOS DATOS CONTENIDOS EN ÉL, SOLO SON DE CARÁCTER TECNICO, POR LO TANTO, NO GENERAN DERECHOS PARA EL O LOS TERRITORIOS DE QUE SE TRATE. DEJANDO A SALVO LOS DERECHOS DE TERCEROS."
-    }
-
-
-
-    dataState = {
-        "type": "Feature",
-        "geometry": {
-            "type": "Polygon",
-            "coordinates": [[
-                [-104.05, 48.99],
-                [-97.22,  48.98],
-                [-96.58,  45.94],
-                [-104.03, 45.94],
-                [-104.05, 48.99]
-            ]]
-        }
+        "Nota": "EL PRESENTE DOCUMENTO Y LOS DATOS CONTENIDOS EN ÉL, SOLO SON DE CARÁCTER TECNICO, POR LO TANTO, NO GENERAN DERECHOS PARA EL O LOS TERRITORIOS DE QUE SE TRATE. DEJANDO A SALVO LOS DERECHOS DE TERCEROS.",
     }
 
     return render(
         request,
-        "sig/view_all_properties.html", 
-        {"dataState":json.dumps(dataState),
-        "dataInmueble": dataInmueble},
+        "sig/view_all_properties.html",
+        {
+            "dataState": json.dumps(dataState),
+            "dataInmueble": dataInmueble,
+        },
     )
