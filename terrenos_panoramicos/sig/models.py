7c33ed24f689fbd61fb5cbadb323e667c5fac63f
Axel Diaz <diaz.ortiz.axel.emmanuel@gmail.com>
2024-02-04 02:02:41 -0600

    stable version with new migration

--- terrenos_panoramicos/sig/models.py
+++ terrenos_panoramicos/sig/models.py
@@ -1,19 +1,25 @@
 from django.db import models
-from users.models import Profile
 from django.contrib.gis.db import models
 from django.core.validators import MaxLengthValidator
 from ventas.models import Inmueble
 
 
-class Perimeter(models.Model):
-    polygon = models.PolygonField()
-    inmueble = models.ForeignKey(
-        Inmueble,
-        on_delete=models.CASCADE,
-        related_name="perimeter",
-        null=True,
-    )
+class ConstructionFrame(models.Model):
+    est = models.IntegerField()
+    pv = models.IntegerField()
+    rumbo = models.PointField()
+    distancia = models.FloatField()
+    v = models.IntegerField()
+    lat = models.FloatField()
+    long = models.FloatField()
+    inmueble = models.ForeignKey(Inmueble, on_delete=models.CASCADE, related_name='construction_frames')
 
+    def __str__(self):
+        return f"ConstructionFrame {self.id} for {self.inmueble}"
+
+class GeoData(models.Model):
+    poligon = models.PolygonField(),
+    inmueble = models.ForeignKey(Inmueble, on_delete=models.CASCADE, related_name='geo_data')
 
 class Position(models.Model):
     point_of_references = models.PointField()
@@ -23,3 +29,46 @@ class Position(models.Model):
         related_name="position",
         null=True,
     )
+
+
+
+class State(models.Model):
+    # No hay campos 'análisis' en Django, así que los he omitido.
+    cve_ent = models.CharField(max_length=255, blank=True)  # Clave de entidad
+    name = models.CharField(max_length=255, blank=True)
+    geometry = models.GeometryField(null=True, blank=True)  # Campo geométrico general
+    centroid = models.PointField(null=True, blank=True)  # Centroide de la entidad
+
+    def __str__(self):
+        return self.name
+
+
+class Municipality(models.Model):
+    state = models.ForeignKey(State, on_delete=models.CASCADE, related_name='municipalities')
+    cv_mun = models.CharField(max_length=255, blank=True)  # Clave de municipio
+    cve_ent = models.CharField(max_length=255, blank=True)  # Clave de entidad
+    name = models.CharField(max_length=255, blank=True)
+    geometry = models.GeometryField(null=True, blank=True)
+    centroid = models.PointField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Colonia(models.Model):
+    municipality = models.ForeignKey(Municipality, on_delete=models.CASCADE, related_name='colonias')
+    cv_mun = models.CharField(max_length=255, blank=True)  # Clave de municipio
+    cve_col = models.CharField(max_length=255, blank=True)  # Clave de colonia
+    name = models.CharField(max_length=255, blank=True)
+    geometry = models.GeometryField(null=True, blank=True)
+    centroid = models.PointField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class PostalCode(models.Model):
+    code = models.CharField(max_length=10, blank=True)
+    municipality = models.ForeignKey(Municipality, on_delete=models.SET_NULL, null=True, blank=True, related_name='postal_codes')
+    
+    def __str__(self):
+        return self.code
--- terrenos_panoramicos/users/models.py
+++ terrenos_panoramicos/users/models.py
@@ -12,22 +12,33 @@ class Profile(models.Model):
 
     Proxy model that extends the base data with other information
     """
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     born = models.DateField(null=True, blank=True)
-
     phone_number = models.CharField(max_length=20, blank=True)
-
     picture = models.ImageField(upload_to="users/pictures", blank=True, null=True)
-
     curp = models.CharField(max_length=18, blank=True)
     ine = models.ImageField(upload_to="users/ine", blank=True, null=True)
     agente_soporte = models.BooleanField("Agente de soporte", default=False)
     gerente_soporte = models.BooleanField("Gerente de soporte", default=False)
-
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.username
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=255)  # 'string' se ha traducido a CharField
+    contract_start = models.DateField()  # 'inicio_de_contrato' es una fecha
+    contract_end = models.DateField()  # 'fin_de_contrato' también es una fecha
+    def __str__(self):
+        return self.name
+
+# Modelo Puestos
+class JobTitle(models.Model):
+    title = models.CharField(max_length=255)  # 'nombre_del_puesto' como el título del puesto
+    person_name = models.CharField(max_length=255)  # 'nombre de la persona' que ocupa el puesto
+    # La conexión a `Client` (llave foránea) se define si `Position` está relacionado con `Client`
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='positions')
+    def __str__(self):
+        return self.title
